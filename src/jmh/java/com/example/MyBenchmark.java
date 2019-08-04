ff3a52c621fa54f8fd9447ff685aba53beb33612
Ho Yan Leung <hy.leung@gmail.com>
2019-08-03 21:11:14 -0700

    Benchmark Jackson JSON serialization

--- src/jmh/java/com/example/MyBenchmark.java
+++ src/jmh/java/com/example/MyBenchmark.java
@@ -4,18 +4,28 @@ import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.State;
 import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import org.openjdk.jmh.infra.Blackhole;
+import java.util.Map;
 
 /**
  * Created by hyleung on 2016-03-02.
  */
 @BenchmarkMode(Mode.AverageTime)
+@State(Scope.Benchmark)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class MyBenchmark {
-
+    private ObjectMapper mapper = new ObjectMapper();
     @Benchmark
-    public void myMethod() {
-
+    public void serializeMapToJson(Blackhole blackhole) throws Exception {
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("key", "value");
+        String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+        blackhole.consume(jsonResult);
     }
 
 }
